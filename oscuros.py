950bf2d030a2f310a921c1d4bdbbea07561d17ee
GaelVM <ajhuneor@outlook.com>
2023-11-29 00:48:39 -0500

    🥷🏼🍻🍺

--- oscuros.py
+++ oscuros.py
@@ -1,6 +1,14 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 import json
+
+# Define la carpeta temporal
+temp_folder = "temp"
+
+# Verifica si la carpeta temporal ya existe, y si no, créala
+if not os.path.exists(temp_folder):
+    os.makedirs(temp_folder)
 
 # URL de la página web
 url = "https://pokemondb.net/go/shadow#shadow-grunts"
@@ -18,8 +26,8 @@ if response.status_code == 200:
 
     # Verificar si se encontró la tabla
     if table:
-        # Lista para almacenar los datos de la tabla como diccionarios
-        table_data = []
+        # Diccionario para almacenar los datos de la tabla
+        all_data_dict = {}
 
         # Iterar sobre las filas de la tabla
         for row in table.find_all('tr'):
@@ -34,29 +42,40 @@ if response.status_code == 200:
                     "Slot1": data[2],
                     "Slot2": data[3],
                     "Slot3": data[4],
-                    # Agregar más columnas según sea necesario
+                    "traduccion": ""  # Puedes inicializarlo con una cadena vacía
                 }
-                table_data.append(row_data)
+
+                # Obtener o inicializar la lista para este tipo
+                type_list = all_data_dict.get(data[0], [])
+                type_list.append(row_data)
+                # Actualizar el diccionario con la lista actualizada
+                all_data_dict[data[0]] = type_list
 
         # Leer el archivo de traducciones
         with open('datos/oscuros.json', 'r', encoding='utf-8') as translation_file:
             translations = json.load(translation_file)
 
         # Agregar traducciones al diccionario original
-        for entry in table_data:
-            matching_translation = next(
-                (t["traduccion"] for t in translations if t["texto"] == entry["texto"]),
-                None
-            )
-            if matching_translation:
-                entry["traduccion"] = matching_translation
+        for entry_list in all_data_dict.values():
+            for entry in entry_list:
+                matching_translation = next(
+                    (t["traduccion"] for t in translations if t["texto"] == entry["texto"]),
+                    None
+                )
+                if matching_translation:
+                    entry["traduccion"] = matching_translation
 
-        # Escribir los datos en un archivo JSON
-        with open('oscuros.json', 'w', encoding='utf-8') as json_file:
-            json.dump(table_data, json_file, ensure_ascii=False, indent=2)
+        # Define la ruta completa del archivo JSON en la carpeta temporal
+        json_file_path = os.path.join(temp_folder, "oscuros.json")
 
-        print("Los datos se han guardado en 'oscuros.json'.")
+        # Guardar el diccionario en un archivo JSON en la carpeta temporal
+        with open(json_file_path, "w", encoding="utf-8") as json_file:
+            json.dump(all_data_dict, json_file, ensure_ascii=False, indent=2)
+
+        print(f"Datos guardados en {json_file_path}")
+
     else:
         print("No se encontró la tabla en la página.")
+
 else:
-    print(f"Error al realizar la solicitud HTTP. Código de estado: {response.status_code}")
+    print(f"Error al obtener la página. Código de estado: {response.status_code}")
