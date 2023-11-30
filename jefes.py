6457656a4627cec99d82e20c5bf30b9d9afc19a4
GaelVM <ajhuneor@outlook.com>
2023-11-29 22:51:47 -0500

    🍺🥷🏼🍻

--- jefes.py
+++ jefes.py
@@ -16,6 +16,10 @@ url = "https://pokemondb.net/go/shadow#shadow-grunts"
 # Realizar la solicitud HTTP
 response = requests.get(url)
 
+# Leer el archivo de datos adicionales
+with open('datos/oscurosdata.json', 'r', encoding='utf-8') as additional_data_file:
+    additional_data = json.load(additional_data_file)
+
 # Verificar si la solicitud fue exitosa (código de estado 200)
 if response.status_code == 200:
     # Analizar el contenido HTML de la página web
@@ -41,16 +45,16 @@ if response.status_code == 200:
                 # Crear un diccionario con los datos de la fila
                 row_data = {
                     "Jefe": data[0],
-                    "Slot1": data[1],
-                    "Slot2": data[2],
-                    "Slot3": data[3],
+                    "Slot1": [{"nombre": item.strip(), "img": additional_data.get(item.strip(), [{}])[0].get("img", "")} for item in data[1].split('\n') if item.strip()],
+                    "Slot2": [{"nombre": item.strip(), "img": additional_data.get(item.strip(), [{}])[0].get("img", "")} for item in data[2].split('\n') if item.strip()],
+                    "Slot3": [{"nombre": item.strip(), "img": additional_data.get(item.strip(), [{}])[0].get("img", "")} for item in data[3].split('\n') if item.strip()],
                 }
 
-                # Obtener o inicializar la lista para este tipo
-                type_list = all_data_dict.get(data[0], [])
-                type_list.append(row_data)
+                # Obtener o inicializar la lista para este jefe
+                boss_list = all_data_dict.get(data[0], [])
+                boss_list.append(row_data)
                 # Actualizar el diccionario con la lista actualizada
-                all_data_dict[data[0]] = type_list
+                all_data_dict[data[0]] = boss_list
 
         # Define la ruta completa del archivo JSON en la carpeta temporal
         json_file_path = os.path.join(temp_folder, "jefes.json")
